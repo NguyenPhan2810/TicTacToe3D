226b79a5daa0c0492e9c0836141604b22ebeee97
NguyenPhanzlxjk <pqn2810@gmail.com>
2020-10-19 08:40:39 +0700

    [Modify]
    Rearrange configuration

--- Source/configuration.py
+++ Source/configuration.py
@@ -1,16 +1,16 @@
+# Colors
 titlesColor = [[124,128, 231]] * 9 + [[190,128, 231]] * 9 + [[128,128, 180]] * 9
 
 for i in range(0, len(titlesColor)):
     titlesColor[i] = [titlesColor[i][0], titlesColor[i][1] + i, titlesColor[i][2]]
     i += 1
 
-
-
 backgroundColor = (51, 51, 51)
 activeTitleColor = (5, 2, 3)
 player1Color = (0, 0, 0)
 player2Color = (255, 255, 255)
 
+# Playground
 nTitles = 3
 titleWiggleAmount = 0.05
 titleWiggleFrequency = 20
@@ -19,9 +19,8 @@ terminalTitleColorChangeAmount = 40
 titlePositionOffset = -0.4
 planeSpacingMultiplier = 2.4
 playGroundScale = 0.3
-heuristicMaxWeigh = 1.1
-heuristicMinWeigh = 1
 
+# Display
 displaySize = (1000, 1000)
 displayAspectRatio = displaySize[0] / displaySize[1]
 FOV = 30
@@ -32,12 +31,14 @@ cameraXRotate = 22
 timePerFrame = 1 / 120
 
 # Min-Max alg
+heuristicMaxWeigh = 1.1
+heuristicMinWeigh = 1
+heuristicWeigh = 2
 maxDepthSearch  = 2
 depthWeight = 10
-heuristicWeigh = 2
 minmaxEvaluationScore = 10000
 
 # MinMaxController
 timeProportionRandomMove = 0.7
-waitingTime = 3.0
+waitingTime = 0.0
 waitingRandomTimeInterval = 0.5
